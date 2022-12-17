6def7f2d1be866ce70c2a61e8805a502a45f2cae
yaoshaoqiang <smxblysq@qq.com>
2022-12-17 11:28:21 +0800

    dddd

--- src/api/user.js
+++ src/api/user.js
@@ -8,11 +8,10 @@ export function login(data) {
   })
 }
 
-export function getInfo(token) {
+export function getInfo() {
   return request({
     url: '/auth/user/info',
-    method: 'get',
-    params: { token }
+    method: 'get'
   })
 }
 
